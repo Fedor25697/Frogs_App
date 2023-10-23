19109c46db09eeda1458a9337f0f9fbd4f7ae53e
Fedor <feda.rotar20000@gmail.com>
2023-10-23 12:03:01 +0300

    App

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
         <Text>New Text</Text>
+        <Text>Reder</Text>
       <StatusBar style="auto" />
     </View>
   );
